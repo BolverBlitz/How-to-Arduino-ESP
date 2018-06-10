4b26208d82ce6a51e3a38ed5776c3accbd471317
BolverBlitz <35345288+BolverBlitz@users.noreply.github.com>
2018-06-10 23:11:57 +0200

    Add files via upload

---
+++ Video 1/YT_Stufe_1.ino
@@ -0,0 +1,38 @@
+//Genutze Bibliotheken
+#include <Adafruit_NeoPixel.h>
+
+//Genutze Variablen
+int button = D2;
+bool status = false;
+
+//RGB Strip Setup
+#define PIN 2
+#define LEDS 50
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDS, PIN, NEO_GRB + NEO_KHZ800);
+
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(button, INPUT_PULLUP);  
+  strip.begin();
+}
+
+
+void loop() {
+ //Button in Seriel
+ status = digitalRead(button);   
+ Serial.println(status);
+
+//Button mit LED
+
+if(status == true) {
+   strip.setPixelColor(0,0,255,0);
+ }else{
+   strip.setPixelColor(0,0,0,0);
+ }
+ strip.show(); 
+
+ 
+}
+
+
